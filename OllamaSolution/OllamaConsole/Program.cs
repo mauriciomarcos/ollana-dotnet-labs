0738e9968962df23720069391ca7bd02314d6df3
Maurício Marcos <001.mmarcos@gmail.com>
2025-09-03 16:50:21 -0300

    Implementação chat Ollama em localhost.

--- OllamaSolution/OllamaConsole/Program.cs
+++ OllamaSolution/OllamaConsole/Program.cs
@@ -1,1 +1,43 @@
 ﻿
+using OllamaSharp;
+
+Console.WriteLine("--- Iniciando Chat com Ollama ---");
+
+var urlOllamaServer = new Uri("http://localhost:11434");
+var ollamaClient = new OllamaApiClient(urlOllamaServer);
+
+// Listar os modelos locais e exibir
+var models = await ollamaClient.ListLocalModelsAsync();
+if (models.Any())
+{
+    foreach (var model in models)
+        Console.WriteLine(model.Name);
+}
+else
+{
+    Console.WriteLine("Nenhum modelo local encontrado. Baixando modelo phi3:latest");
+
+    var retunInstallModel = ollamaClient.PullModelAsync("phi3:latest");
+    await foreach (var item in retunInstallModel)
+    {
+        Console.WriteLine($"Status da instalação do modelo: {Math.Round((decimal)item?.Percent!)}%");
+    }
+
+    Console.WriteLine("Modelo phi3:latest instalado com sucesso");
+}
+
+// Selecionando o modelo a ser utilzado
+ollamaClient.SelectedModel = "phi3:latest";
+
+// Interagindo com o modelo
+Console.WriteLine("Ask your question...");
+
+var ollamaChat = new Chat(ollamaClient);
+var userPrompt = Console.ReadLine() ?? string.Empty;
+
+await foreach (var answer in ollamaChat.SendAsync(userPrompt))
+{
+    Console.Write(answer);
+}
+
+Console.WriteLine("--- Fim ---");
